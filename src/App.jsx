29db2e528827fa90168dda6c86d2a40cccb83340
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-28 17:17:27 -0300

    salvando dados de login no localhost

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import PublicRoutes from './routes/public_routes'
 function App() {
   const { auth } = useContext(AuthContext)
   const getAuth = localStorage.getItem('login')
-  console.log(getAuth)
 
   return getAuth || auth ? <PrivateRoutes /> : <PublicRoutes />
 }
